7b5307e2373edf50463769ce0f3af397f37009ce
Alexandr Ershov <ershov.avdd@gmail.com>
2022-11-20 00:39:17 +0700

    Added Task schedule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { AuthModule } from "./auth/auth.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { PrismaService } from "./prisma/prisma.service";
 import { ScheduleModule } from "./schedule/schedule.module";
+import { ScheduleModule as NestScheduleModule } from "@nestjs/schedule";
+import { TasksModule } from "./tasks/task.module";
 
 @Module({
-  imports: [AuthModule, ScheduleModule, PrismaModule],
+  imports: [AuthModule, ScheduleModule, PrismaModule, NestScheduleModule.forRoot(), TasksModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
